4add3072b164b84adcba212a3659169f715e383c
veilleuxd <veilleuxd@educ.college-montreal.qc.ca>
2022-09-14 08:54:46 -0400

    commit initial

---
+++ main.py
@@ -0,0 +1,18 @@
+# 14/9/2022   D/M/Y
+# David A. Veilleux gr 405
+
+
+def count_words(phrase):
+    # on compte le nombre d'espaces et d'apostrophes
+    espaces = (phrase.count(" "))
+    apostrophe = (phrase.count("'"))
+
+    # le nombre de mots est toujours 1 de plus que le nombre d'espaces et d'apostrophes
+    # on trouve le nombre de mots
+    return apostrophe + espaces + 1
+
+# demande à l'utilisateur d'écrire une phrase. cette phrase sera enregistré comme un string sous la variable Count_Word
+nombre_mots = count_words(input("Écris-moi une phrase. Je vais compter ses mots!\n--> "))
+
+# le code dit a la personne le nombre de mots dans sa phrase
+print("Le nombre de mots est " + str(nombre_mots) + "!")
